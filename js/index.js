f6dedc16f28729817e00f76268c6625664ca0cea
Bharat Nagda <nagdabharat@gmail.com>
2020-06-28 06:18:37 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+console.log('JavaScript is loaded');
+
 const a = document.getElementByID('main');
 console.log(a);
 a.addEventListener('click', (event) => {
