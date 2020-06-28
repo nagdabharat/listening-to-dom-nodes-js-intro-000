a45f69d9b56266a9e329404136c69e17f93b63df
Bharat Nagda <nagdabharat@gmail.com>
2020-06-28 06:09:29 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const main = document.querySelector('#main');
+main.addEventListener('click', (event) => {
+  alert('I was clicked');
+});
