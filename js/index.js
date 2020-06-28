16cf382a11b3fc85317b5e9ce512e8fad21a1801
Bharat Nagda <nagdabharat@gmail.com>
2020-06-28 07:27:48 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click',bubble)
 }
 */
-
+let divs = document.querySelectorAll('div');
 function capture(e) {
   console.log(this.firstChild.nodeValue.trim() + ' captured');
 }
