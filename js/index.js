63ef8bf2504b713c46a7659a5e4f0d2ec659d6bf
Bharat Nagda <nagdabharat@gmail.com>
2020-06-28 07:27:36 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ for (let i = 0; i < divs.length; i++) {
 function capture(e) {
   console.log(this.firstChild.nodeValue.trim() + ' captured');
 }
- 
+
 for (let i = 0; i < divs.length; i++) {
   // set the third argument to `true`!
   divs[i].addEventListener('click', capture, true);
